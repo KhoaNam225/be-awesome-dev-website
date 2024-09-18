92dd989c14d9d52df4d3e6ac82a1ece9362ac5d3
Nam Pham <pknam25@gmail.com>
2024-09-18 22:07:47 +0800

    Added syntax highlighter and format the chat message

--- front-end/app/components/message.tsx
+++ front-end/app/components/message.tsx
@@ -1,15 +1,54 @@
 'use client'
 
+import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import { ChatMessage } from '../models/chat'
+import { robotoMono } from '../utils/fonts'
 
 export type ChatMessageProps = {
   message: ChatMessage
 }
 
+const CodeBlock = ({ node, inline, className, children, ...props }: any) => {
+  const match = /language-(\w+)/.exec(className || '')
+  return match ? (
+    <SyntaxHighlighter
+      {...props}
+      PreTag="div"
+      language={match[1]}
+      style={oneDark}
+    >
+      {String(children).replace(/\n$/, '')}
+    </SyntaxHighlighter>
+  ) : (
+    <code
+      className={`${className} px-2 py-4 rounded-lg ${
+        !inline ? 'block text-pretty my-3' : 'inline'
+      } ${robotoMono.className}`}
+      {...props}
+    >
+      {children}
+    </code>
+  )
+}
+
+const Paragraph = ({ children, ...props }: any) => {
+  return <p className="my-4">{children}</p>
+}
+
 export default function Message({ message }: ChatMessageProps) {
   return (
     <div className={getMessageClassName(message)}>
-      <p className="text-pretty leading-relaxed">{message.content}</p>
+      <ReactMarkdown
+        className="text-pretty leading-relaxed"
+        components={{
+          code: CodeBlock,
+          p: Paragraph,
+        }}
+      >
+        {message.content}
+      </ReactMarkdown>
     </div>
   )
 }
